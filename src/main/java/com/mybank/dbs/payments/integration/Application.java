5dfb69f4a0bbf2bd464c41bbe009c64d7aca0d93
Jasper Linschoten <jasper@backbase.com>
2021-02-24 09:30:44 +0100

    Add doc section tags.

--- src/main/java/com/mybank/dbs/payments/integration/Application.java
+++ src/main/java/com/mybank/dbs/payments/integration/Application.java
@@ -7,11 +7,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// tag::add-import-annotation[]
 @SpringBootApplication
 @EnableDiscoveryClient
 @Import({PaymentOrdersController.class, OrderExecutor.class })
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
+// end::add-import-annotation[]
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
